f21f14d445d5b112a8cb81a27a227e281d504bbc
Vadim Kozenkov <vadimoclock@gmail.com>
2022-11-06 17:19:32 +0300

    add algorithm, rework class

--- src/StudentStatsCLI/Main.java
+++ src/StudentStatsCLI/Main.java
@@ -1,7 +1,61 @@
 package StudentStatsCLI;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Objects;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
     public static void main(String[] args) {
+        ArrayList<Student> students = new ArrayList<>();
+        System.out.println("Формат ввода данных о студенте: \"ФИО, номер группы, номер студенческого билета\"\n");
 
+        do {
+            System.out.println("Введите информацию о студенте: (для завершения работы программы введите \"end\")");
+
+            Scanner scanner = new Scanner(System.in);
+            String inputText = scanner.nextLine();
+
+            if (Objects.equals(inputText, "end")) {
+                System.out.println("Список студентов:");
+                for (Student student : students) {
+                    System.out.println("- " + student);
+                }
+                break;
+            }
+
+            String[] splitText = inputText.split(" ");
+            if (splitText.length < 5
+                    || isNumeric(splitText[0])
+                    || isNumeric(splitText[1])
+                    || isNumeric(splitText[2])
+                    || !isNumeric(splitText[4])) {
+                System.out.println("Некорректно введеное поле");
+            } else {
+                Student student = new Student(splitText[0],
+                        splitText[1],
+                        splitText[2],
+                        splitText[3],
+                        Integer.parseInt(splitText[4]));
+
+                if (students.contains(student))
+                    System.out.println("Студент с такими данными уже есть");
+                else {
+                    students.add(student);
+                    System.out.printf("Студент %d успешно добавлен.\n", student.getStudentId());
+                }
+            }
+        } while (true);
+    }
+
+    public static boolean isNumeric(String s) {
+        try {
+            Integer.parseInt(s);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
--- src/StudentStatsCLI/Student.java
+++ src/StudentStatsCLI/Student.java
@@ -1,20 +1,33 @@
 package StudentStatsCLI;
 
+import java.util.Objects;
 import java.util.UUID;
 
 public class Student {
     private String name;
     private String surname;
     private String patronymic;
-    private int groupNumber;
-    private final UUID studentId;
+    private String groupNumber;
+    private final Integer studentId;
 
-    public Student(String name, String surname, String patronymic, int groupNumber) {
+    public Student(String name, String surname, String patronymic, String groupNumber) {
         this.name = name;
         this.surname = surname;
         this.patronymic = patronymic;
         this.groupNumber = groupNumber;
-        this.studentId = UUID.randomUUID();
+        this.studentId = (int) (Math.round(Math.random() * 1000000));
+    }
+
+    public Student(String name, String surname, String patronymic, String groupNumber, Integer studentId) {
+        this.name = name;
+        this.surname = surname;
+        this.patronymic = patronymic;
+        this.groupNumber = groupNumber;
+        this.studentId = studentId;
+    }
+
+    public Integer getStudentId() {
+        return studentId;
     }
 
     public String getName() {
@@ -41,11 +54,33 @@ public class Student {
         this.patronymic = patronymic;
     }
 
-    public int getGroupNumber() {
+    public String getGroupNumber() {
         return groupNumber;
     }
 
-    public void setGroupNumber(int groupNumber) {
+    public void setGroupNumber(String groupNumber) {
         this.groupNumber = groupNumber;
+    }
+
+    @Override
+    public String toString() {
+        return  name + " " +
+                surname + " " +
+                patronymic + ", " +
+                groupNumber + ", " +
+                studentId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(studentId, student.studentId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname, patronymic, groupNumber, studentId);
     }
 }
